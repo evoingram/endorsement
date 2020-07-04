248f80d3c17c2685e4427d01cd3e929892ae4301
Erica L. Ingram <inquiries@aquoco.co>
2020-07-04 13:34:52 -0700

    CTCI 2.2 problem solved

--- C2LinkedLists/python/2.1-answer.py
+++ C2LinkedLists/python/2.1-answer.py
@@ -12,34 +12,34 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
-def remove_dups(self):
-    # create cache
-    cache = set()
-    current_head = self.head
-    if current_head is None:
-        return None
-    previous_head = None
-    while current_head: 
-        pass
-        # if is in cache, skip
-        if current_head.data in cache:
-            previous_head.next = current_head.next 
-        # if not in cache, add node to cache
-        else:
-            cache.add(current_head.data)
-            previous_head = current_head
-        current_head = current_head.next
-
-def remove_dups_FU(self):
-    current_head = self.head
-    if current_head is None:
-        return None
-    previous_head = None
-    while current_head:
-        previous_head = current_head
-        while previous_head.next:
-            if previous_head.next.data == current_head.data:
-                previous_head.next = previous_head.next.next
+    def remove_dups(self):
+        # create cache
+        cache = set()
+        current_head = self.head
+        if current_head is None:
+            return None
+        previous_head = None
+        while current_head: 
+            pass
+            # if is in cache, skip
+            if current_head.data in cache:
+                previous_head.next = current_head.next 
+            # if not in cache, add node to cache
             else:
-                previous_head = previous_head.next
-            current_head = current_head.next 
+                cache.add(current_head.data)
+                previous_head = current_head
+            current_head = current_head.next
+
+    def remove_dups_FU(self):
+        current_head = self.head
+        if current_head is None:
+            return None
+        previous_head = None
+        while current_head:
+            previous_head = current_head
+            while previous_head.next:
+                if previous_head.next.data == current_head.data:
+                    previous_head.next = previous_head.next.next
+                else:
+                    previous_head = previous_head.next
+                current_head = current_head.next 
