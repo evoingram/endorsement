210be8a2a643fbe8a98017b65448443579933508
Erica L. Ingram <inquiries@aquoco.co>
2020-07-04 13:49:42 -0700

    CTCI 2.3 problem solved

--- C2LinkedLists/python/2.3-answer.py
+++ C2LinkedLists/python/2.3-answer.py
@@ -4,3 +4,15 @@
     # given only access to that node.
 
 # (i.e. any node but the first and last, not necessarily the exact middle)
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None 
+    
+    def delete_middle(self, middle):
+        if middle.next is None:
+            return None
+        placeholder = middle.next
+        middle.data = placeholder.data
+        middle.next = placeholder.next
