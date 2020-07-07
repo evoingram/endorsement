1237c870304d818da27472fcd495663880fad4b5
Erica L. Ingram <inquiries@aquoco.co>
2020-07-06 20:36:45 -0700

    CTCI 4.10 problem solved

--- C4TreesGraphs/python/4.10-answer.py
+++ C4TreesGraphs/python/4.10-answer.py
@@ -5,3 +5,19 @@
 # A tree T2 is a subtree of T1 if there exists a node in T1 such that 
     # the subtree of n is identical to T2.
 # That is, if you cut off the tree at node n, the two trees would be identical.
+
+
+class BinarySearchTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def check_subtree(self, root1, root2):
+        if root1:
+            if root2:
+                if root1 == root2:
+                    check_subtree(root1.left, root2.left)
+                    check_subtree(root1.right, root2.right)
+                    return True 
+        return False
