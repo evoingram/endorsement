2817f52b0b7b2fc4000cecb182152c879c250244
Erica L. Ingram <inquiries@aquoco.co>
2020-07-06 18:03:08 -0700

    CTCI 4.2 problem solved

--- C4TreesGraphs/python/4.2-answer.py
+++ C4TreesGraphs/python/4.2-answer.py
@@ -3,3 +3,32 @@
 # Given a sorted (increasing order) array with unique integer elements, write an algorithm to 
     # create a binary search tree with minimal height.
 
+class BinarySearchTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    # Insert the given value into the tree
+    def insert(self, value):
+        
+        # if value is less than current value, go left
+        if value < self.value:
+            
+            if not self.left:
+                self.left = BinarySearchTree(value)
+            else:
+                self.left.insert(value)
+
+        # if value is greater than/equal to current value, go right
+        elif value >= self.value:
+
+            if not self.right:
+                self.right = BinarySearchTree(value)
+            else:
+                self.right.insert(value)
+    
+    def minimal_tree(self, array):
+        length = len(array)
+        for x in range(0, length):
+            self.insert(value)
