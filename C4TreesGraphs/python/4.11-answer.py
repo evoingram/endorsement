f4b58359119510f270c033d1a8003d0ca9c85af8
Erica L. Ingram <inquiries@aquoco.co>
2020-07-06 20:57:40 -0700

    CTCI 4.11 problem solved

--- C4TreesGraphs/python/4.11-answer.py
+++ C4TreesGraphs/python/4.11-answer.py
@@ -6,4 +6,31 @@
 # All nodes should be equally likely to be chosen.
 # Design and implement an algorithm for getRandomNode, and explain how you would implement
     # the rest of the methods.
+from random import *
+
+class BinarySearchTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+        self.length = 0
+
+    def getRandomNumber(self):
+        return random(0, self.length)
+
+    def getRandomNode(self, root):
+        possible_nodes = []
+        if root:
+            self.length += 1
+            possible_nodes.append(root)
+            getRandomNode(root.left)
+            getRandomNode(root.right)
+        length = len(possible_nodes)
+        random = self.getRandomNumber()
+        for x in range(0, length):
+            if x == random:
+                return possible_nodes[x]
+
+        
+
     
