16dbb513440338ffb9ace2ae7a279e89759e4952
Erica L. Ingram <inquiries@aquoco.co>
2020-07-29 22:17:37 -0700

    added extra info to linked lists

--- SamplesDSAlgos/data structures/datastructures-linkedlist_singly.py
+++ SamplesDSAlgos/data structures/datastructures-linkedlist_singly.py
@@ -1,10 +1,36 @@
 
 """
-
+What is the difference between an array and a linked list?
+    Arrays use memory differently.
+    Arrays store and index elements contiguously.
+    Each element of linked list is stored in a node.
+        Each node has reference or pointer to next node.
+    Linked lists describe lists of things in recursive fashion.
+    Arrays describe lists of things in iterative fashion.
+    LLs are easier to insert into & delete from middle of LL than array.
+    LLs are not as cache friendly since caches are typically 
+        optimized for contiguous memory accesses.
+    LLs don't need to be allocated with a static amount of memory up front.
+    You can keep adding elements to linked lists as much as you want; 
+        can't with arrays.
 """
 # SINGLY LINKED LIST
-# time complexity:   O()
-# space complexity:  O()
+# made of bunch of nodes that point to next one in list
+# every node has two properties: 
+    # value of whatever is being stored
+    # pointer to next node in list
+# adding and removing is easy; change next pointer on previous node (O(n)))
+    # similar to arrays
+# commonly used for holding lists of data
+    # certain cases when better than array
+
+# time complexity:   Avg   | Worst
+    # Access:       O(n)   |   O(n)
+    # Search:       O(n)   |   O(n)
+    # Insertion:    O(1)   |   O(1)
+    # Deletion:     O(1)   |   O(1)
+
+# space complexity:  O(n)
 
 class Node:
     def __init__(self, value=None, next_node=None):
@@ -12,9 +38,11 @@ class Node:
         self.value = value
         # reference to the next node in the list
         self.next_node = next_node
+    # return value of current node
     def get_value(self):
         return self.value
-
+    
+    # return next node
     def get_next(self):
         return self.next_node
 
