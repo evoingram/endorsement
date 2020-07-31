64fe62a4125c20d2ed4ee06ab345957a5d2ec8f8
Erica L. Ingram <inquiries@aquoco.co>
2020-07-30 23:00:39 -0700

    radix sort removed pseudocode

--- SamplesDSAlgos/sort/sort_radix.py
+++ SamplesDSAlgos/sort/sort_radix.py
@@ -11,33 +11,6 @@ Counting sort can’t be used if a range of key value is large (suppose range is
     so radix sort is the best choice to sort in linear time.
 '''
 
-'''
-pseudocode:
-    Radix-Sort(A, d)
-    //It works same as count sort for d number of passes.
-    //Each key in A[1..n] is a d-digit integer.
-    //(Digits are numbered 1 to d from right to left.)
-        for j = 1 to d do
-            //A[]-- Initial Array to Sort
-            int count[10] = {0};
-            //Store the count of "keys" in count[]
-            //key- it is number at digit place j
-            for i = 0 to n do
-            count[key of(A[i]) in pass j]++
-            for k = 1 to 10 do
-            count[k] = count[k] + count[k-1]
-            //Build the resulting array by checking
-            //new position of A[i] from count[k]
-            for i = n-1 downto 0 do
-            result[ count[key of(A[i])] ] = A[j]
-            count[key of(A[i])]--
-            //Now main array A[] contains sorted numbers
-            //according to current digit place
-            for i=0 to n do
-            A[i] = result[i]
-        end for(j)
-'''
-
 # RADIX SORT:
     # only used to sort numbers
     # sort numbers from least to most significant digit
