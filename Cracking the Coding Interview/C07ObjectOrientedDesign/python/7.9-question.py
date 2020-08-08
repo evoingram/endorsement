378ffbbe4a02ebd7051a256ea555f6942a0adde4
Erica L. Ingram <inquiries@aquoco.co>
2020-08-07 21:41:50 -0700

    7.9 circular array ctci problem solved

--- Cracking the Coding Interview/C07ObjectOrientedDesign/python/7.9-question.py
+++ Cracking the Coding Interview/C07ObjectOrientedDesign/python/7.9-question.py
@@ -9,3 +9,26 @@
 # CircularArray
     # items, head
     # convert (index), rotate (shift_right), get (i), set(i, item)
+
+class CircularArray:
+    def __init__(self, head, items=[]):
+        self.items = items
+        self.head = head
+    
+    def convert(self, index):
+        if index == 0:
+            index += self.items.length
+
+    def rotate(self, shift_right):
+        head = self.convert(shift_right)
+    
+    def get(self, i):
+        if i < 0 or i >= self.items.length:
+            return "error"
+        item_index = self.convert(i)
+        return items[item_index]
+
+    def set(self, i, item):
+        item_index = self.convert(i)
+        items[item_index] = item
+        
