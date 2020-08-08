0ab9654e4e8d630142baa1ed7bfa30ea347ebdf1
Erica L. Ingram <inquiries@aquoco.co>
2020-08-07 20:54:43 -0700

    othello 7.8 CTCI problem solved

--- Cracking the Coding Interview/C07ObjectOrientedDesign/python/7.8-question.py
+++ Cracking the Coding Interview/C07ObjectOrientedDesign/python/7.8-question.py
@@ -9,3 +9,99 @@
     # The win is assigned to the person with the most pieces.
 # Implement the object-oriented design for Othello.
 
+# Game (Singleton) 
+    # makes it easy to call method within game class w/o having to pass around references to game object
+    # but can only be substantiated once
+
+
+# game
+    # players, game_instance, board, rows, columns
+    # direction left right up down
+    # color white black
+
+    # get_instance, get_board
+class Game:
+    def __init__(self, game_instance, board, rows=10, columns=10, players=[]):
+        self.players = players
+        self.game_instance = game_instance
+        self.board = board
+        self.rows = rows
+        self.columns = columns
+        self.direction = {
+            left: "left",
+            right: "right",
+            up: "up",
+            down: "down"
+        }
+        self.color = {
+            black: "black", 
+            white: "white"
+        }
+
+    def get_instance(self):
+        return self.game_instance
+
+    def get_board(self):
+        return self.board
+
+# board
+    # black_count, white_count, board
+
+    # initialize, place_color (row, column, color), flip_section (row, column, color, direction), 
+        # get_score_for_color (color), update_score (new_color, new_pieces)
+class Board:
+    def __init__(self, black_count, white_count, board):
+        self.black_count = black_count
+        self.white_count = white_count
+        self.board = board
+
+    def initialize(self):
+        pass
+
+    def place_color(self, row, column, color):
+        pass
+
+    def flip_section(self, row, column, color, direction):
+        pass
+
+    def get_score_for_color(self, color):
+        if color == "white":
+            return self.white_count
+        return self.black_count
+
+    def update_score(self, new_color, new_pieces):
+        pass
+
+# piece
+    # color 
+
+    # flip, get_color
+class Piece:
+    def __init__(self, color):
+        self.color = color
+
+    def flip(self):
+        if self.color == "black":
+            self.color = "white"
+        else:
+            self.color = "black"
+
+    def get_color(self):
+        return self.color
+
+# player
+    # color
+
+    # get_score, play_piece, get_color
+class Player:
+    def __init__(self, color):
+        self.color = color
+
+    def get_score(self):
+        pass
+
+    def play_piece(self):
+        pass
+
+    def get_color(self):
+        return self.color
--- Cracking the Coding Interview/C07ObjectOrientedDesign/python/7.9-question.py
+++ Cracking the Coding Interview/C07ObjectOrientedDesign/python/7.9-question.py
@@ -1,6 +1,11 @@
 # 7.9 Circular Array
 
-# Implement a CircularArray class that supports an array-like data structure which can be efficiently rotated.
-# If possible, the class should use a generic type (also called a template) and should support iteration via the standard for
+# Implement a CircularArray class that supports an array-like data 
+    # structure which can be efficiently rotated.
+# If possible, the class should use a generic type (also called a 
+    # template) and should support iteration via the standard for
     # (Obj o:  circularArray) notation.
 
+# CircularArray
+    # items, head
+    # convert (index), rotate (shift_right), get (i), set(i, item)
