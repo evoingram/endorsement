4e4afd77e805bc67834c0dc1abc0142df58d1f68
Erica L. Ingram <inquiries@aquoco.co>
2020-08-10 01:12:31 -0700

    set up rest of OOAD problems

--- Cracking the Coding Interview/C07ObjectOrientedDesign/questions/other-questions.py
+++ Cracking the Coding Interview/C07ObjectOrientedDesign/questions/other-questions.py
@@ -52,18 +52,7 @@ Customization: How would users make special requests on their room? What kind of
 Cancellation / Modification: How would the system treat booking cancellation (within the allowed time period)? What about other changes? What types of modifications would be covered?
 '''
 
-'''
-7. Design a Parking Lot
-
-This is one of the most common OOD interview questions and a must-know.
-
-The interviewer will want to hear you discuss:
-
-Payment Flexibility: How are customers able to pay at different points (i.e. either at the customer’s info console on each floor or at the exit) and by different methods (cash, credit, coupon)?
-Capacity: How will the parking capacity of each lot be considered? What happens when a lot becomes full?
-Vehicle Types: How will capacity be allocated for different parking spot types — e.g. motorcycles, compact cars, electric cars, handicap vehicles, etc.?
-Pricing: How will pricing be handled? It should accommodate having different rates for each hour. For example, customers have to pay $4 for the first hour, $3.5 for the second and third hours, and $2.5 for all the remaining hours.
-'''
+# 7. Parking Lot done already
 
 '''
 8. Design an Online Stock Brokerage System
