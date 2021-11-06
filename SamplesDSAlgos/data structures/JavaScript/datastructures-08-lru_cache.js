4af9a536684f9be467e8d2687e97a5f63b500bdf
Erica L. Ingram <inquiries@aquoco.co>
2021-11-06 00:52:31 -0700

    feat: [] more Leetcode DSA problems/review

--- SamplesDSAlgos/data structures/JavaScript/datastructures-08-lru_cache.js
+++ SamplesDSAlgos/data structures/JavaScript/datastructures-08-lru_cache.js
@@ -216,13 +216,6 @@ class LRUCache {
         this.storage = {};
     }
     get = (key) => {
-        if (!key in this.storage) return null;
-        let listValue = this.storage[key];
-        if (listValue !== null) {
-            
-        }
-    }
-    get = (key) => {
         if (!key in this.storage) return null
         let listValue = this.storage[key]
         if (listValue != null) {
@@ -230,33 +223,24 @@ class LRUCache {
             this.nodeList.moveToFront(lrucNode)
             return listValue
         }
-        // needs to move the key-value pair to the end of the order such that 
-        // the pair is considered most-recently used
         if (key in this.keys) {
-            // Retrieves the value associated with the given key
             index = this.keys.index(key)
             this.keys.pop(index)
             this.keys.push(key)
-            // Returns the value associated with the key
             return this.storage[key]
         }
-        // Returns null if the key-value pair doesn't exist in the cache.
         else return null
     }
     set = (key, value) => {
-        // Adds the given key-value pair to the cache. 
-        // The newly- added pair should be considered the most-recently used entry in the cache.
         if (!key in this.storage) {
             this.storage[key] = value;
             this.node_list.addToHead(key);
         }
-        // Additionally, in the case that the key already exists in the cache, we simply want to overwrite the old value associated with the key with the newly-specified value.
         else {
             this.storage[key] = value;
             lruc_node = this.node_list.find(key)
             this.node_list.move_to_front(lruc_node)
         }
-        // If the cache is already at max capacity before this entry is added, then the oldest entry in the cache needs to be removed to make room.
         if (this.node_list.length > this.max) {
             delete this.storage[this.node_list.tail.value]
             this.node_list.remove_from_tail()
