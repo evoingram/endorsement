0a276fd188ed2dcc9837a9378cbe01b6722d7afb
Erica L. Ingram <inquiries@aquoco.co>
2021-11-27 17:59:13 -0800

    feat: [] DSA practice binary/linear search

--- SamplesDSAlgos/search/javascript/search-02-linear.js
+++ SamplesDSAlgos/search/javascript/search-02-linear.js
@@ -19,22 +19,12 @@ linearSearch1 = (arr, target) => {
    return -1;
 }
 
-linearSearch2 = (arr, target) => {
-   for (let x = 0; x < arr.length - 1; x++) if (arr[x] === target) return x;
-   return false;
-}
-
-linearSearch3 = (arr, target) => {
-   for (let x = 0; x < arr.length - 1; x++) if (arr[x] === target) return x;
-   return false;
-};
-
-let linearSearch = (arr, target) => {
+linearSearch = (arr, target) => {
    for (let x = 0; x < arr.length; x++) if (arr[x] === target) return x;
    return -1;
 };
 
-// linearSearch = (arr, target) = {};
+// linearSearch = (arr, target) => {};
 
 
 array = [4, 22, 41, 40, 27, 30, 36, 16, 42, 37, 14, 39, 3, 6, 34, 9, 21, 2, 29, 47]
@@ -43,5 +33,5 @@ console.log(`find 40 (index 3):  ${linearSearch(array, 40)}`);
 console.log(`find 39 (index 11):  ${linearSearch(array, 39)}`);
 console.log(`find 34 (index 14):  ${linearSearch(array, 34)}`);
 console.log(`find 36 (index 6):  ${linearSearch(array, 36)}`);
-console.log(`find 35 (not in array):  ${linearSearch(array, 35)}`);
-console.log(`find 50 (not in array):  ${linearSearch(array, 50)}`);
+console.log(`find 35 (not in array, -1):  ${linearSearch(array, 35)}`);
+console.log(`find 50 (not in array, -1):  ${linearSearch(array, 50)}`);
